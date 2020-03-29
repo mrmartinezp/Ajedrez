ffbf8fcc9b3d0a8c368759de4786531257eb7e23
Rosa <mmartinezp@ieslavereda.es>
2020-03-29 18:16:01 +0200

    Creo un objeto

---
+++ eclipse-workspace/Ficha/src/Principal/prueba.java
@@ -0,0 +1,18 @@
+package Principal;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rosa
+ */
+public class prueba {
+    public static void main(String[] args) {
+   
+    System.out.println("Hola");
+    }
+}
